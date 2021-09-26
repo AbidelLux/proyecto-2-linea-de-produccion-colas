15191a0388f15199620e6d37b4a80659ae9a0b98
victor abdiel lux juracan <3031591380108@ingenieria.usac.edu.gt>
2021-09-26 15:37:13 -0600

    interfaz completa primera

--- Interfaz.py
+++ Interfaz.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import  messagebox
 from tkinter import filedialog
+from tkinter import ttk
 root  = Tk()
-
+#------------------------------------------------------------------------------------------------------------------------
 def infoEstud():
     messagebox.showinfo("Estudiante","Victor Abdiel Lux Juracán\n201403946\nIPC2")
 def infoProgram():
@@ -18,30 +19,51 @@ def abrirXMLmaq():
 def abrirXMLsim():
     ruta = filedialog.askopenfilename(title="Abrir",filetypes=(("archivos XML","*.xml"),("todos los Archivos","*.*")))
     print(ruta)
-
+#----------------------------------------------------------------------------------------------------------------------- raiz de interfaz
 root.title('Simulacion')
 #root.resizable(0,0)#ancho, largo (boolean redimencion)
 root.iconbitmap("ICO.ico") #icono
 #root.geometry("650x350")#cambiar tama
 barraMenu = Menu(root)
 root.config(bg="sky blue", menu=barraMenu)#configuraciones
-
+#------------------------------------------------------------------------------------------------------------------------ Frame
 fr = Frame()
 fr.pack()
 fr.config(bg="white",width="650",height="350")
 #Imagen = PhotoImage(file="image.png")
 #label1 = Label(fr,image=Imagen) #para usar label variable
 #label1.place(x="100",y="200")
+#-----------------------------------------------------------------------------------------------------------------------componentes frame
+Label(fr,text = "Tiempo de Simulacion",font=("Arial","12"),bg="white").grid(row=4,column=2)
+texto = Entry(fr,font=("Arial","12"))
+texto.grid(row=4, column=4)
 
-Label(fr,text = "Tiempo de Simulacion",font=("Arial","12"),bg="white").grid(row="0",column="0")
-texto = Entry(fr,font=("Arial","18"))
-texto.grid(row="0", column="1")
 
-Label(fr,text="Productos Disponibles para simulacion",font=("Arial Black","14"),bg="white").grid(row="0",column="0")
+Label(fr,text="Productos Disponibles para simulacion",font=("Arial Black","14"),bg="white").grid(row=0,column=0)
+combo = ttk.Combobox(fr,values=["prod2","prod1"],state="readonly")
+combo.grid(row=1,column=0)
+combo.bind("<<ComboboxSelected>>")
+
+Label(fr,text="Componentes Necesarios",font = ("Arial Black","14"),bg="white").grid(row=2,column=0)
 Componentes = Text(fr)
-Componentes.grid(row=3,column=0,stycky="e",padx=10,pady=10)
-Scrollbar(fr,command=Text.yview)
+Componentes.grid(row=3,column=0)
+scroll1 = Scrollbar(fr,command=Componentes.yview)
+scroll1.grid(row=3,column=1,sticky="nsew")
+Componentes.config(width=45,height=10,padx=10,pady=10,yscrollcommand=scroll1.set)
 
+Label(fr,text="Tabla de Resultados de Simulacion",font=("Arial Black","14"),bg="white").grid(row=2,column=2)
+table = Text(fr)
+table.grid(row=3,column=2)
+scroll2 = Scrollbar(fr,command=table.yview)
+scroll2.grid(row=3,column=3,sticky="nsew")
+table.config(width=45,height=10,padx=10,pady=10,yscrollcommand=scroll2.set)
+
+barraprogreso = ttk.Progressbar(fr,mode="indeterminate")
+barraprogreso.grid(row=4,column=0)
+barraprogreso.start(5)
+
+
+#-----------------------------------------------------------------------------------------------------------------------MEnu
 archivoMenu = Menu(barraMenu,tearoff=0)
 archivoMenu.add_command(label="Cargar Archivo Maquina",command=abrirXMLmaq)
 archivoMenu.add_command(label="Cargar Archivo Simulacion",command=abrirXMLsim)
